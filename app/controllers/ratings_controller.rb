8651dd4db9183a023fb0c0b5c083b7c8bf0cff2f
Jason Novack <jason.novack@gmail.com>
2013-01-28 12:43:17 -0600

    added sort by when_rated, consolidated into dropdown box

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -83,5 +83,6 @@ class RatingsController < ApplicationController
     fff = Rating.search(params[:search])
     @ratings_by_name = fff.paginate(:order => 'name ASC', :page => params[:page], :per_page =>10)
     @ratings_by_score = fff.paginate(:order => 'score DESC', :page => params[:page], :per_page =>10)
+    @ratings_by_when_rated = fff.paginate(:order => 'when_rated DESC', :page => params[:page], :per_page =>10)
   end
 end
